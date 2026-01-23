1de0397988dc1d3244d54b10195166058f5eceb9
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2026-01-23 14:47:17 -0500

    Add a static icons directory with placeholder icons (change later) to test setting window icons and executable icons

--- mandala/gui/runner.py
+++ mandala/gui/runner.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 import logging
+from pathlib import Path
 
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
 from ..config.loggers import initialize_logging
@@ -11,22 +13,25 @@ from .mainwindow import MandalaMainWindow
 
 logger = logging.getLogger(__name__)
 
+basedir = Path(__file__).parent.parent.parent
+window_icon = str(basedir / "icons" / "windowIcon.png")
+
+try:  # Windows only for taskbar icon
+    from ctypes import windll
+
+    myappid = "wonyoungjang.mandala.random_file_copier.0.0.1"
+    windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+except ImportError:
+    pass
+
 
 def main() -> None:
     """Run the application."""
     initialize_logging()
-
     logger.info("Start: Mandala GUI")
 
     app = QApplication()
-
-    gui = MandalaMainWindow()
-    gui.show()
-
+    app.setWindowIcon(QIcon(window_icon))
+    w = MandalaMainWindow()
+    w.show()
     app.exec()
-
-    logger.info("End: Mandala GUI")
-
-
-if __name__ == "__main__":
-    main()
