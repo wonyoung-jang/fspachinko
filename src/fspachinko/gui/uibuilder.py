cf5ebacb7754defb80af021f70580e85d2b058ff
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2026-03-15 09:16:38 -0400

    Simplify ui_builder with a tuple enumerating all widgets for the build method

--- src/fspachinko/gui/uibuilder.py
+++ src/fspachinko/gui/uibuilder.py
@@ -1,6 +1,6 @@
 """Main module."""
 
-from PySide6.QtWidgets import QVBoxLayout
+from PySide6.QtWidgets import QVBoxLayout, QWidget
 
 from ..constants import ByteUnit, TimeUnit
 from .components import (
@@ -25,46 +25,38 @@ class UIBuilder:
         self.root = PathSelectorWidget("Root", "root")
         self.dest = PathSelectorWidget("Destination", "dest")
         self.filecount = FileCountWidget("File count", "filecount")
-        self.dircreator = DirectoryCreateWidget("Create directories", "directory")
-        self.filenamer = FilenamerWidget("Filenamer", "filenamer")
-        self.dirname_filter = TextFilterWidget("Directory name", "dirname")
-        self.keyword_filter = TextFilterWidget("Keyword", "keyword")
-        self.extension_filter = TextFilterWidget("Extension", "extension")
-        self.filesize_filter = RangeFilterWidget("File Size", "filesize", tuple(ByteUnit))
-        self.duration_filter = RangeFilterWidget("Duration", "duration", tuple(TimeUnit))
+        self.directory = DirectoryCreateWidget("Create directories", "directory")
+        self.filename = FilenamerWidget("Filenamer", "filenamer")
+        self.dirname = TextFilterWidget("Directory name", "dirname")
+        self.keyword = TextFilterWidget("Keyword", "keyword")
+        self.extension = TextFilterWidget("Extension", "extension")
+        self.filesize = RangeFilterWidget("File Size", "filesize", tuple(ByteUnit))
+        self.duration = RangeFilterWidget("Duration", "duration", tuple(TimeUnit))
         self.options = OptionsWidget("Options", "options")
         self.logging = LogWidget("logging")
         self.progress = ProgressWidget("progress")
-        self.has_config: tuple[BaseGroupBox, ...] = (
+        self.all_widgets: tuple[QWidget, ...] = (
             self.root,
             self.dest,
             self.filecount,
-            self.dircreator,
-            self.filenamer,
-            self.dirname_filter,
-            self.keyword_filter,
-            self.extension_filter,
-            self.filesize_filter,
-            self.duration_filter,
+            self.directory,
+            self.filename,
+            self.dirname,
+            self.keyword,
+            self.extension,
+            self.filesize,
+            self.duration,
             self.options,
+            self.logging,
+            self.progress,
         )
+        self.has_config: tuple[BaseGroupBox, ...] = tuple(w for w in self.all_widgets if isinstance(w, BaseGroupBox))
 
     def build(self) -> QVBoxLayout:
         """Set up the main UI layouts."""
         layout = QVBoxLayout()
-        layout.addWidget(self.root)
-        layout.addWidget(self.dest)
-        layout.addWidget(self.filecount)
-        layout.addWidget(self.dircreator)
-        layout.addWidget(self.filenamer)
-        layout.addWidget(self.dirname_filter)
-        layout.addWidget(self.keyword_filter)
-        layout.addWidget(self.extension_filter)
-        layout.addWidget(self.filesize_filter)
-        layout.addWidget(self.duration_filter)
-        layout.addWidget(self.options)
-        layout.addWidget(self.logging)
-        layout.addWidget(self.progress)
+        for w in self.all_widgets:
+            layout.addWidget(w)
         return layout
 
     @property
