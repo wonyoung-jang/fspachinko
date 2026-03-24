9224666c50c99ca225077c779e2f9de05471bf5d
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2026-03-24 08:43:51 -0400

    Update vulture whitelist

--- whitelist.py
+++ whitelist.py
@@ -1,6 +1,21 @@
 """Whitelist for vulture."""
 
+import fspachinko
+import fspachinko.configuration._defaultconfig
+from fspachinko.configuration.model import ConfigModel
 from fspachinko.entrypoints.gui.components import PathSelectorWidget
 
+fspachinko.hello  # noqa: B018
 PathSelectorWidget.dragEnterEvent  # noqa: B018
 PathSelectorWidget.dropEvent  # noqa: B018
+ConfigModel.filecount.validate_filecount_model  # noqa: B018
+ConfigModel.directory.validate_count  # noqa: B018
+ConfigModel.directory.validate_directory_model  # noqa: B018
+ConfigModel.filename.validate_template  # noqa: B018
+ConfigModel.duration.validate_range_filter_model  # noqa: B018
+ConfigModel.duration.validate_minimum  # noqa: B018
+ConfigModel.duration.validate_maximum  # noqa: B018
+ConfigModel.options.validate_rng_seed  # noqa: B018
+ConfigModel.options.validate_max_per_dir  # noqa: B018
+ConfigModel.validate_root_and_dest_paths  # noqa: B018
+fspachinko.configuration._defaultconfig.DEFAULT_CONFIG  # noqa: B018, SLF001
